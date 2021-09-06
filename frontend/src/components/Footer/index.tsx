a20c0a06248bf29a19bc15403a59eb6a78774ba6
Abhner Vianna <abineh@gmail.com>
2021-09-06 14:49:15 -0300

    Componentes estáticos básicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/abineh" target="_blank" rel="noreferrer">Abhner Vianna</a></p>
+                <p className="text-light"><small><strong>Semana Spring React SDS4</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
